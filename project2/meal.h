80e2e5106b9f082a08b13c40eaa2efbe14f902e0
SUNIT3105 <126572449+SUNIT3105@users.noreply.github.com>
2023-02-28 17:20:02 +0530

    Add files via upload
    done using c programing

---
+++ project2/meal.h
@@ -0,0 +1,8 @@
+
+
+int breakfast(int i);
+int lunch(int i);
+int snacks(int i);
+int dinner(int i);
+//meal.h
+//Displaying dinner.c.
---
+++ project2/snacks.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int snacks(int i) {
+    printf("\n\nMENU:\n1. Coffee\n2. Tea\n3. Samosa\n4. Vada\n5. Maggi\n6. Exit");
+    printf("\n\n\t\tTotal Cost: Rs%d/-",i=30);
+    return (i=30);
+}
+//snacks.c
+//Displaying dinner.c.
